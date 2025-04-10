5df8da69e060da8a25d187f23b91c3f2a67c7056
fatima <timaaron17@gmail.com>
2025-04-10 14:38:01 +0200

    Modifications pour la redirection vers la page d'accueil

--- scanner/include/header.php
+++ scanner/include/header.php
@@ -1,4 +1,8 @@
 <?php
+// Message de redirection
+echo "Redirection en cours... Veuillez patienter.";
+
+// Rediriger vers la page souhaitée
 header("Location: frontend/index.php");
 exit();
 ?>
--- scanner/index.php
+++ scanner/index.php
@@ -1,4 +1,9 @@
 <?php
+// Afficher un message de redirection
+echo "Vous êtes en train d'être redirigé vers la page d'accueil...";
+
+// Rediriger vers la page souhaitée
 header("Location: frontend/index.php");
 exit();
 ?>
+
